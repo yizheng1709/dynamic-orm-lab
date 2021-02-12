6f91320fb1f9efc1696b539b4761fa8aa2a8fa9b
Eva <yi.zheng1709@gmail.com>
2021-02-12 11:08:47 -0500

    commit tree 5f84f73fd56e2b1a06741199a5de5a58cebfd01b

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,68 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+    
+
+    def self.table_name
+       "#{self.to_s.downcase.pluralize}"
+    end 
+
+    def self.column_names
+    
+        sql = "pragma table_info('#{table_name}')"
+    
+        table_info = DB[:conn].execute(sql)
+        column_names = []
+        table_info.each do |row|
+          column_names << row["name"]
+        end
+        column_names.compact
+    end
+
+    def initialize(hash={})
+        hash.each do |property, value|
+            # binding.pry
+            self.send("#{property}=", value)
+        end 
+    end 
+
+    def values_for_insert
+        values = []
+        self.class.column_names.each do |col_name|
+          values << "'#{send(col_name)}'" unless send(col_name).nil?
+        end
+        values.join(", ")
+        # binding.pry
+      end
+    
+      def col_names_for_insert
+        self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    #   binding.pry
+    end
+    
+
+    def table_name_for_insert
+        self.class.table_name
+      end
+
+    def save
+        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+        DB[:conn].execute(sql)
+       
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+     
+    def self.find_by_name(name)
+        sql = "SELECT #{self.column_names.join(", ")} FROM #{self.table_name} WHERE name = '#{name}'"
+        DB[:conn].execute(sql)
+    end
+
+    def self.find_by(hash)
+        # binding.pry
+        sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0]} = ? "
+        
+        a = DB[:conn].execute(sql, hash.values[0])
+        # binding.pry
+    end 
   
 end
--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,23 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end 
+
+    # def save 
+    #     sql = <<-SQL
+    #     INSERT INTO students(name, grade)
+    #     VALUES (?, ?)
+    #     SQL
+
+    #     DB[:conn].execute(sql, self.name, self.grade)
+    #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    #     row = DB[:conn].execute("SELECT id, name, grade FROM students WHERE id = ?", self.id)[0]
+    #     @name = row[1]
+    #     @grade = row[2]
+        
+        
+    # end 
+
 end
